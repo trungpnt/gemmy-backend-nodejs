2e8e5dc1e9bb3493c5f859a97a198a5a383707ac
Trung <trungpnt0605@gmail.com>
2021-06-14 20:27:39 +0700

    changed class model

--- backend/models/class.js
+++ backend/models/class.js
@@ -1,41 +1,41 @@
 const mongoose = require("mongoose");
 
 const studentSchema = new mongoose.Schema({
-  student_id: String,
-  dob: Date,
-  contact_info: String,
-  test_date : Date,
-  class_level_enrolled: String, //class code,
-  discount_code : String,
-  tuition_fee : String,
-  notes : String
+    student_id: String,
+    dob: Date,
+    contact_info: String,
+    test_date: Date,
+    class_level_enrolled: String, //class code,
+    discount_code: String,
+    tuition_fee: String,
+    student_notes: String
 });
 
 const classSessionSchema = new mongoose.Schema({
-  day: String,
-  time_range: String
+    day: String,
+    time_range: String
 })
 
 const classSchema = mongoose.Schema({
-  class_name: { type: String, required: true },
-  //functionize this class code
-  /*năm(21)+45(level)+Mar(tháng hiện tại)+A(label)+preIelts(className) => 2145MARApreIelts
-năm hiện tại,2 số đầu, tháng ( init 1st time only ), label_name,className
-backend */
-  class_code: { type: String, required: true },
-  //
-//   creator: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  //
-  slots : { type: String, required: true },
-  number_of_sessions: { type: String, required: true },
-  date_start: { type: Date, required: true },
-  //remember to calculate
-  date_end: { type: Date, required: true },
-  is_active: { type: Boolean, required: true },
+    class_name: { type: String, required: true },
+    //functionize this class code
+    /*năm(21)+45(level)+Mar(tháng hiện tại)+A(label)+preIelts(className) => 2145MARApreIelts
+  năm hiện tại,2 số đầu, tháng ( init 1st time only ), label_name,className
+  backend */
+    class_code: { type: String, required: true },
+    //
+    //   creator: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    //
+    slots: { type: String, required: true },
+    number_of_sessions: { type: String, required: true },
+    date_start: { type: Date, required: true },
+    //remember to calculate
+    date_end: { type: Date, required: true },
+    is_active: { type: Boolean, required: true },
 
-  class_session: [classSessionSchema],
-  student_list: [studentSchema]
-
+    class_session: [classSessionSchema],
+    student_list: [studentSchema],
+    class_note : {type: String, required: false}
 });
 
 
