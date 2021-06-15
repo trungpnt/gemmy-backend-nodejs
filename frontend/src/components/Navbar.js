1d29d18932d8c295c5907053d4dd8daa9d679fde
Trung <trungpnt0605@gmail.com>
2021-06-15 17:50:48 +0700

    added display flex justify content align-items for navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,6 +3,7 @@ import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
 import { Link } from 'react-router-dom';
 import { SidebarData } from './SidebarData';
+import './Navbar.css'
 
 function Navbar() {
 
