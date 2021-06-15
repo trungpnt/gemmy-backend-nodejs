54536c55bb7dfee14b11bb150e67efb04ef91f93
Trung <trungpnt0605@gmail.com>
2021-06-15 17:14:31 +0700

    added nav-menu-items and navbar-toggle

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
-  return (
-    <div className="App">
-      hi
-    </div>
-  );
+    return (
+        <>
+            <Router>
+                <Navbar />
+                <Switch>
+                    <Route path='/' />
+                </Switch>
+            </Router>
+        </>
+    );
 }
 
 export default App;
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,15 +1,24 @@
 import React from 'react'
-import * as FaIcons  from "react-icons/fa";
+import * as FaIcons from "react-icons/fa";
 import { Link } from 'react-router-dom';
 
 function Navbar() {
     return (
-        <div className='navbar'>
-            <Link to="#" className='menu-bars'>
-                <FaIcons.FaBars/>
-            </Link>
-        </div>
-    )
+        <>
+            <div className='navbar'>
+                <Link to="#" className='menu-bars'>
+                    <FaIcons.FaBars />
+                </Link>
+            </div>
+            <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+                <ul className='nav-menu-items'>
+                    <li className="navbar-toggle">
+                        
+                    </li>
+                </ul>
+            </nav>
+        </>
+    );
 }
 
-export default Navbar
+export default Navbar;
