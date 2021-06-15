91b1b147afdd47b3db96d554cb27bfddf4118c52
Trung <trungpnt0605@gmail.com>
2021-06-15 17:31:07 +0700

    added Sidebar Data using array

---
+++ frontend/src/components/SidebarData.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
+
+export const SidebarData = [
+    {
+        title: 'Home',
+        path : '/',
+        icon: <AiIcons.AiFillHome/>,
+        //short for class name
+        cName : 'nav-text'
+    }
+]
