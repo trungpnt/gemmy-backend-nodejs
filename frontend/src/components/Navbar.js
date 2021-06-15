31e1ee452c83d8da811c289d2a9d6a58f5acea9a
Trung <trungpnt0605@gmail.com>
2021-06-15 17:17:00 +0700

    added AiIcons component for close

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
 import { Link } from 'react-router-dom';
 
 function Navbar() {
@@ -13,7 +14,9 @@ function Navbar() {
             <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
                 <ul className='nav-menu-items'>
                     <li className="navbar-toggle">
-                        
+                        <Link>
+                            <AiIcons.AiOutlineClose/>
+                        </Link>
                     </li>
                 </ul>
             </nav>
