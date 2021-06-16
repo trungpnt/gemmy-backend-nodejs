05294f61bf7168e65d33fec5f3ff16db4f42190c
Trung <trungpnt0605@gmail.com>
2021-06-17 02:38:50 +0700

    added update new student to class.student_list, handled 3 scenarios that a class can be updated

--- backend/controllers/class.js
+++ backend/controllers/class.js
@@ -65,65 +65,64 @@ exports.updateClass = (req, res, next) => {
 
     //Update a full class
     if (req.body.class_label != null && req.body.class_level != null && req.body.class_name) {
-        
+
         Class.findById(req.params.id)
             .then(class_found => {
-                if(class_found){
+                if (class_found) {
                     var class_code = genClassCode(req.body.class_level, req.body.class_label, req.body.class_name);
 
-                const classModel = new Class({
-                    _id: req.body.id,
-                    class_name: req.body.class_name,
-                    class_code: class_code,
-                    slots: req.body.slots,
-                    tuition_fee: req.body.tuition_fee,
-                    total_sessions: req.body.total_sessions,
-                    date_start: req.body.date_start,
-                    date_end: "06/05/1990",
-                    note: req.body.note,
-                    is_active: req.body.is_active,
+                    const classModel = new Class({
+                        _id: req.body.id,
+                        class_name: req.body.class_name,
+                        class_code: class_code,
+                        slots: req.body.slots,
+                        tuition_fee: req.body.tuition_fee,
+                        total_sessions: req.body.total_sessions,
+                        date_start: req.body.date_start,
+                        date_end: "06/05/1990",
+                        note: req.body.note,
+                        is_active: req.body.is_active,
 
-                    class_session: req.body.class_session,
-                    student_list: req.body.student_list,
+                        class_session: req.body.class_session,
+                        student_list: req.body.student_list,
 
-                    current_total_students: req.body.student_list.length,
-                    remaining_slots: req.body.slots - req.body.student_list.length
-                });
-
-                class_found = classModel;
-
-                Class.findOne({ class_code: class_code })
-                    .then(result => {
-                        if (result) {
-                            res.status(200).json({ message: "The class_code with the same name already exists !" });
-                        } else {
-
-
-                            Class.updateOne({ _id: classModel._id }, classModel)
-                                .then(result => {
-                                    if (result.n > 0) {
-                                        res.status(200).json({
-                                            message: "Update new class successfully",
-                                            updated_class: classModel
-                                        });
-                                    } else {
-                                        res.status(401).json({ message: "Not authorized!" });
-                                    }
-                                })
-                                .catch(error => {
-                                    res.status(500).json({
-                                        message: "Couldn't update Class!"
-                                    });
-                                });
-                        }
-                    })
-                    .catch(error => {
-                        res.status(501).json({
-                            message: "Please check your inputs and try again!"
-                        });
+                        current_total_students: req.body.student_list.length,
+                        remaining_slots: req.body.slots - req.body.student_list.length
                     });
+
+                    class_found = classModel;
+
+                    Class.findOne({ class_code: class_code })
+                        .then(result => {
+                            if (result) {
+                                res.status(200).json({ message: "The class_code with the same name already exists !" });
+                            } else {
+
+                                Class.updateOne({ _id: classModel._id }, classModel)
+                                    .then(result => {
+                                        if (result.n > 0) {
+                                            res.status(200).json({
+                                                message: "Update new class successfully",
+                                                updated_class: classModel
+                                            });
+                                        } else {
+                                            res.status(401).json({ message: "Not authorized!" });
+                                        }
+                                    })
+                                    .catch(error => {
+                                        res.status(500).json({
+                                            message: "Couldn't update Class!"
+                                        });
+                                    });
+                            }
+                        })
+                        .catch(error => {
+                            res.status(501).json({
+                                message: "Please check your inputs and try again!"
+                            });
+                        });
                 }
-                else{
+                else {
                     res.status(404).json({ message: "Class not found!" });
                 }
             })
@@ -131,6 +130,69 @@ exports.updateClass = (req, res, next) => {
                 res.status(500).json({
                 })
             })
+    }
+
+    //UPDATE STUDENT => ADD NEW STUDENT TO THIS CLASS
+    else if (req.body.student_list != null && req.body.is_active == null && req.body.class_session == null) {
+        Class.findById(req.params.id)
+            .then(class_found => {
+                if (class_found) {
+                    //Logic to add new student
+                    var current_list = class_found.student_list;
+                    var list_size = req.body.student_list.length;
+                    //Check if new number of students exceed current slots
+                    if (list_size > class_found.remaining_slots) {
+                        res.status(401).json({
+                            message: "The class slot is full",
+                            remaining_slots: class_found.remaining_slots,
+                            new_slot_added: list_size
+                        })
+                    }
+                    else {
+                        //otherwise
+                        for (var i = 0; i < list_size; i++) {
+                            current_list.push(req.body.student_list[i]);
+                        }
+                        var new_size = current_list.length;
+                        //Update student_list
+                        class_found.student_list = current_list;
+
+                        class_found.current_total_students = new_size;
+                        //Update the remaining slots
+                        class_found.remaining_slots = class_found.slots - new_size;
+
+                        Class.updateOne({ _id: class_found._id }, class_found)
+                            .then(result => {
+                                if (result.n > 0) {
+                                    res.status(200).json({
+                                        message: "Update new student list successfully!",
+                                        initial_slots: class_found.slots,
+                                        new_slot_added : list_size,
+                                        remaining_slots: class_found.remaining_slots,
+                                        total_current_students: class_found.current_total_students,
+                                        new_student_list: class_found.student_list,
+                                    });
+                                } else {
+                                    res.status(401).json({ message: "Not authorized!" });
+                                }
+                            })
+                            .catch(error => {
+                                res.status(500).json({
+                                    message: "Couldn't update Class!"
+                                });
+                            });
+                    }
+
+                } else {
+                    res.status(404).json({ message: "Class not found!" });
+                }
+            })
+            .catch(error => {
+                res.status(500).json({
+                    message: "Fetching Class failed!"
+                });
+            });
+
     }
 
     //UPDATE JUST THE CLASS ACTIVE STATUS
@@ -164,7 +226,6 @@ exports.updateClass = (req, res, next) => {
                     message: "Fetching Class failed!"
                 });
             });
-
     }
 };
 
